4c30661c7fde643627e0119e4dff515a6edc37df
Your Name <Khalilcheguenni@gmail.com>
2021-01-29 14:01:08 +0100

    first commit

---
+++ Filter.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="w-1/2 h-screen sm:auto md:auto lg:auto shadow-2xl mx-auto flex flex-col items-center">
+      <div className="py-5">
+        <span className="  text-5xl font-light text-white">Movie</span>
+        <span className="  text-5xl font-light py-2 px-2 text-red-600 ">
+          Finder
+        </span>
+      </div>
+    
+    </div>
+  );
+}
+
+export default App;
